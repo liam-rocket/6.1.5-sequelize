752321dedbe76ee1d316b8c0bcdacff3376857b0
Liam Leung <liam.leung@binery.co>
2022-06-11 11:37:54 +0800

    commit

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 /* eslint-disable import/extensions */
-import express from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import ItemRouter from './routes/item.routes.js';
+import ItemRouter from "./routes/item.routes.js";
 
-const PORT = 3005;
-const envFilePath = '.env';
+const PORT = process.env.PORT || 3004;
+const envFilePath = ".env";
 dotenv.config({ path: path.normalize(envFilePath) });
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
-app.use(express.static('dist'));
+app.use(express.static("public"));
+app.use(express.static("dist"));
 
 const routers = [ItemRouter];
-routers.forEach((router) => app.use('/', router));
+routers.forEach((router) => app.use("/", router));
 
 app.listen(PORT);
 console.log(`🚀 App listening on the port ${PORT}`);
